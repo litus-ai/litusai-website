a2143b41e1558163e261c44ff1a58146d9278cc2
Valahaar <niccolo.campolungo@gmail.com>
2023-06-13 13:17:23 +0000

    testing new github action

--- next.config.js
+++ next.config.js
@@ -1,28 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   experimental: {
     appDir: true,
   },
-  output: 'export',
-  images: {
-    unoptimized: true,
-  }
 };
 
 module.exports = nextConfig;
